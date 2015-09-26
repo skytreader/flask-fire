4e5fa7ddaa9bfe4fcacf852b8aafac475fe3318f
Chad Estioco <chadestioco@gmail.com>
2015-09-27 02:11:03 +0800

    This slipped past my inspection.

--- app/tests/factories.py
+++ app/tests/factories.py
@@ -1,57 +1,21 @@
 from faker import Faker
-from librarian import models
-from librarian.tests.fakers import BookFieldsProvider
+from app import models
 from sqlalchemy import orm
 
 import factory
 import factory.alchemy
-import librarian
+import app
 import random
 import sqlalchemy
 
 fake = Faker()
-fake.add_provider(BookFieldsProvider)
 
-class LibrarianFactory(factory.alchemy.SQLAlchemyModelFactory):
+
+class UserFactory(factory.alchemy.SQLAlchemyModelFactory):
     class Meta:
-        model = models.Librarian
-        sqlalchemy_session = librarian.db.session
+        model = models.User
+        sqlalchemy_session = app.db.session
 
     id = factory.Sequence(lambda n: n)
     username = factory.LazyAttribute(lambda x: fake.user_name())
     password = factory.LazyAttribute(lambda x: fake.password())
-
-
-class GenreFactory(factory.alchemy.SQLAlchemyModelFactory):
-    class Meta:
-        model = models.Genre
-        sqlalchemy_session = librarian.db.session
-
-    id = factory.Sequence(lambda n: n)
-    name = factory.Sequence(lambda n: "Genre%s" % n)
-    creator = factory.LazyAttribute(lambda x: LibrarianFactory().id)
-
-
-class BookCompanyFactory(factory.alchemy.SQLAlchemyModelFactory):
-    class Meta:
-        model = models.BookCompany
-        sqlalchemy_session = librarian.db.session
-
-    id = factory.Sequence(lambda n: n)
-    name = factory.LazyAttribute(lambda x: fake.company())
-    creator = factory.LazyAttribute(lambda x: LibrarianFactory().id)
-
-
-class BookFactory(factory.alchemy.SQLAlchemyModelFactory):
-    class Meta:
-        model = models.Book
-        sqlalchemy_session = librarian.db.session
-
-    id = factory.Sequence(lambda n: n)
-    isbn = factory.LazyAttribute(lambda x: fake.isbn())
-    title = factory.LazyAttribute(lambda x: fake.title())
-    genre = factory.LazyAttribute(lambda x: GenreFactory().id)
-    printer = factory.LazyAttribute(lambda x: BookCompanyFactory().id)
-    publisher = factory.LazyAttribute(lambda x: BookCompanyFactory().id)
-    publish_year = factory.LazyAttribute(lambda x: fake.year())
-    creator = factory.LazyAttribute(lambda x: LibrarianFactory().id)
