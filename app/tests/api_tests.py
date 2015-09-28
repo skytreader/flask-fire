bae9476f760fdf10b396d3df63c19876e47a1d73
Chad Estioco <chadestioco@gmail.com>
2015-09-29 04:11:50 +0800

    Add API tests.

--- app/tests/api_tests.py
+++ app/tests/api_tests.py
@@ -2,6 +2,7 @@ from base import AppTestCase
 from faker import Faker
 from flask.ext.login import login_user
 
+import dateutil.parser
 import factory
 import flask.ext.login
 import json
@@ -16,3 +17,9 @@ class ApiTests(AppTestCase):
     
     def setUp(self):
         super(ApiTests, self).setUp()
+
+    def test_servertime(self):
+        servertime = self.client.get("/api/util/servertime")
+        self.assertEquals(servertime._status_code, 200)
+        data = json.loads(servertime.data)
+        self.assertTrue(dateutil.parser.parse(data["now"]))
