023ef46d068c1baa3e2bb4c3dcbbec3e2dc4a9d4
Chad Estioco <chadestioco@gmail.com>
2015-10-13 18:53:20 +0800

    Test travis.

--- config.py
+++ config.py
@@ -14,8 +14,8 @@ TESTING = bool(os.environ.get("TESTING"))
 import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))  
 
-SQLALCHEMY_DATABASE_URI = 'mysql://root@127.0.0.1/alexandria'
-SQLALCHEMY_TEST_DATABASE_URI = 'mysql://root@127.0.0.1/alexandria_test'
+SQLALCHEMY_DATABASE_URI = 'mysql://root@127.0.0.1/app'
+SQLALCHEMY_TEST_DATABASE_URI = 'mysql://root@127.0.0.1/app_test'
 SQLALCHEMY_ECHO = DEVEL
 DATABASE_CONNECT_OPTIONS = {"user":"root"}
 
