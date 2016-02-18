a8998619c8ccfe94ac9b4059ba7999d2d83e457c
Chad Estioco <chadestioco@gmail.com>
2016-02-18 15:49:31 +0800

    Hahahaha senseless copy-pasted default.

--- main/models.py
+++ main/models.py
@@ -49,12 +49,13 @@ class User(Base, UserMixin):
     username = db.Column(db.String(50), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False)
     can_read = db.Column(db.Boolean, nullable=False, default=False,
-      server_default=db.func.current_timestamp())
+      server_default=False)
     can_write = db.Column(db.Boolean, nullable=False, default=False,
-      server_default=db.func.current_timestamp())
+      server_default=False)
     can_exec = db.Column(db.Boolean, nullable=False, default=False,
-      server_default=db.func.current_timestamp())
-    is_user_active = db.Column(db.Boolean, nullable=False, default=True)
+      server_default=False)
+    is_user_active = db.Column(db.Boolean, nullable=False, default=True,
+      server_default=True)
 
     def __init__(self, **kwargs):
         self.username = kwargs["username"]
