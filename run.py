85d7aba6f4591b1197c601b804ab4c38cbb0daed
Chad Estioco <chadestioco@gmail.com>
2017-03-28 12:22:04 +0800

    wew librarian artifact.

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
-import librarian
+import main
 from config import APP_HOST, APP_PORT, DEVEL
 
 if __name__ == "__main__":
-    librarian.init_blueprints()
-    librarian.init_db()
-    librarian.app.run(host=APP_HOST, port=APP_PORT, debug=DEVEL)
+    main.init_blueprints()
+    main.init_db()
+    main.app.run(host=APP_HOST, port=APP_PORT, debug=DEVEL)
